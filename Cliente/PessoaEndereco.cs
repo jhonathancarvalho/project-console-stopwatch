0212314ab3fc2ed9759e2f8efd78bfa965e77b7f
Tiago Pereira Cardoso <ti.dev.cardoso@gmail.com>
2023-12-05 11:12:22 +0100

    Inserindo valores de pessoas e enderecos

---
+++ Cliente/PessoaEndereco.cs
@@ -0,0 +1,17 @@
+﻿namespace Cliente
+{
+    public class PessoaEndereco
+    {
+        public string CPF { set; get; }
+
+        public string Endereco { set; get; }
+
+        public int Numero { set; get; }
+
+        public string Cidade { set; get; }
+
+        public string Estado { set; get; }
+
+        public string CEP { set; get; }
+    }
+}
--- Executar/Program.cs
+++ Executar/Program.cs
@@ -1,54 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Cliente;
-
-Console.WriteLine("Hello, World!");
+﻿using Cliente;
 
 var Pessoas = new List<Pessoa>() 
 { 
     new Pessoa() { CPF = "12030926795", Nome = "Tiago Pereira Cardoso", Nascimento = DateTime.Parse("1989-12-26") },
-    new Pessoa() { CPF = "12847566708", Nome = "Jhonathan Pereira", Nascimento = DateTime.Parse("1993-01-04") }
+    new Pessoa() { CPF = "12847566708", Nome = "Jhonathan Pereira", Nascimento = DateTime.Parse("1993-01-04") },
+    new Pessoa() { CPF = "10198942702", Nome = "Ana Luiza Cortes", Nascimento = DateTime.Parse("1984-09-09") }
 };
 
-var generica = "Tiago";
-
-string texto = "1234 meu nome e qiwjeiqwjeip 4.1 true List";
-int numero = 50;
-bool verdairoOuFalso = true;
-
-Console.WriteLine("oiaoisoaisoaisoias");
-Console.WriteLine(123123123);
-Console.WriteLine(generica + texto + numero + verdairoOuFalso);
-
-Console.WriteLine($"Ola eu sou {generica}, recebo {numero} e nunca minto {verdairoOuFalso}.");
-
-var minhaLista = new List<string>() { "ola", "ixi" };
-
-minhaLista.Add("texto 1");
-minhaLista.Add("Caramba muito legal");
-minhaLista.Add("Lascou!!!");
-
-for (var i = 0; i < minhaLista.Count; i++)
+var Endereco = new List<PessoaEndereco>() 
 {
-    if (minhaLista[i].Equals("ixi"))
-    {
-        Console.WriteLine($"O indice de {minhaLista[i]} e {i}");
-
-        // Se o cliente tem saldo devedor, mostrar mensagem na tela de cobranca
-        if(i == 1)
-        {
-            Console.WriteLine("Pague sua fatura em atraso");
-        }
-    }
-}
-
-foreach (var item in minhaLista)
-{
-    Console.WriteLine(item);
-}
+    new PessoaEndereco() { CPF = "12030926795", Endereco = "Via Luigi Chiereguin", Numero = 27, Estado = "TV", Cidade = "Treviso", CEP = "31100" },
+    new PessoaEndereco() { CPF = "12030926795", Endereco = "Av Cristovao Colombo", Numero = 75, Estado = "ES", Cidade = "Serra", CEP = "29163172" },
+    new PessoaEndereco() { CPF = "12847566708", Endereco = "Vista do mestre", Numero = 167, Estado = "ES", Cidade = "Serra", CEP = "29163000" },
+};
 
 foreach (var pessoa in Pessoas)
 {
-    Console.WriteLine($"Meu nome e {pessoa.Nome} e meu CPF e {pessoa.CPF} e meu nascimento e {pessoa.Nascimento}.");
-}
+    var End = Endereco.Where(x => x.CPF.Equals(pessoa.CPF));
 
-Console.WriteLine(Pessoas);
+    Console.WriteLine($"{pessoa.Nome} tem ( {End.Count()} ) enderecos cadastrados no sistema");
+
+    int cnt = 1;
+    foreach (var item in End)
+    {
+        Console.WriteLine($"{pessoa.Nome} - Endereco {cnt}: {item.Endereco} {item.Numero}, {item.Cidade} {item.Estado} - CEP/CAP: {item.CEP}");
+        cnt++;
+    }
+}
